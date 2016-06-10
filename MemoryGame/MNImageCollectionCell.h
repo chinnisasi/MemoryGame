24bfa07a7f06ab03d2b85770223b6c4179dfd052
Sasidhar Koti <chinnisasi@gmail.com>
2016-06-10 23:03:01 +0530

    adding library files

---
+++ MemoryGame/MNImageCollectionCell.h
@@ -0,0 +1,15 @@
+//
+//  MNImageCollectionCell.h
+//  MemoryGame
+//
+//  Created by Sasidhar Koti on 10/06/16.
+//  Copyright © 2016 Sasidhar Koti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MNImageCollectionCell : UICollectionViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *imgView;
+@property (weak, nonatomic) IBOutlet UIView *backGroundView;
+
+@end
---
+++ MemoryGame/MNImageCollectionCell.m
@@ -0,0 +1,20 @@
+//
+//  MNImageCollectionCell.m
+//  MemoryGame
+//
+//  Created by Sasidhar Koti on 10/06/16.
+//  Copyright © 2016 Sasidhar Koti. All rights reserved.
+//
+
+#import "MNImageCollectionCell.h"
+
+
+@interface MNImageCollectionCell ()
+@end
+
+@implementation MNImageCollectionCell
+
+-(void)prepareForReuse{
+    [super prepareForReuse];
+}
+@end
