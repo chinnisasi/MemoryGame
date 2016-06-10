7fa55498f31ed2de38a8b9b54435904e845d0109
Sasidhar Koti <sasidharkoti@sasidhar-wooqer.local>
2016-06-10 12:15:32 +0530

    Initial Commit

---
+++ MemoryGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MemoryGame
+//
+//  Created by Sasidhar Koti on 10/06/16.
+//  Copyright © 2016 Sasidhar Koti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MemoryGame/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MemoryGame
+//
+//  Created by Sasidhar Koti on 10/06/16.
+//  Copyright © 2016 Sasidhar Koti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
